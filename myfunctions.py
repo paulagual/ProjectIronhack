ccd60b8328bde9b3479d6833b8a567b025daab12
Paula Gual <paulagual@gmail.com>
2019-11-12 20:05:07 +0100

    EDA numerical vars

--- myfunctions.py
+++ myfunctions.py
@@ -17,3 +17,25 @@ def desc_num(x, bins=100, name='column'):
 
     box = sns.boxplot(x=x).set_title(f'Boxplot of {name}')
     plt.show()
+    
+    
+def desc_cat(df,col, name='column'):
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    import math
+    '''
+    Describe categorical vars and draw bar chart
+    input= dataframe column
+    outut= describe of the column values, bar chart
+    '''
+    null = df[col].isnull().sum()
+    print("Numbers of Nulls: ", null)
+    values =  df[col].value_counts()
+    print(values)
+    plt.subplots(figsize=(10, 6))
+    bar = df.groupby(col)['fullVisitorId'].count().reset_index()
+    bar.columns = [col,'count']
+    sns.barplot(x=col, y='count', data=bar)
+    plt.title(f'Bar chart {name}')
+    plt.xticks(rotation=90)
+    plt.show()
